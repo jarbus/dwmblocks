4275953dde4409f831c6bbd67ce38030b25aff35
jarbus <jarbus@tutanota.com>
2020-05-26 22:20:12 -0400

    better blocks

--- blocks.h
+++ blocks.h
@@ -1,19 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	//{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
 
-	{"", "~/bin/statusbar/volume",						0,		10},
+  //{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
+  {"", "weather",             0, 10},
+	{"", "internet",						0,		10},
+	{"", "volume",						0,		10},
+
+	{"☀", "light | sed 's/\\..*//g'",					0,		11},
+
+	{"", "battery",						5,		0},
 
 	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
 
-	{"", "~/bin/statusbar/clock",						5,		0},
+	{"", "clock",						5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
